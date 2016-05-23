78572ac8cb0910d72e3b1c1e96e2c35cd88ff4cd
coljamkop <coljamkop@gmail.com>
2016-05-23 11:57:57 -0600

    Initial commit

---
+++ app/src/main/java/coljamkop/tabtest/MyFamilyRecyclerViewAdapter.java
@@ -0,0 +1,77 @@
+package coljamkop.tabtest;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import coljamkop.tabtest.FamilyViewFragment.OnFamilyListFragmentInteractionListener;
+import coljamkop.tabtest.Content.FamilyContent.Family;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link Family} and makes a call to the
+ * specified {@link OnFamilyListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyFamilyRecyclerViewAdapter extends RecyclerView.Adapter<MyFamilyRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Family> mValues;
+    private final OnFamilyListFragmentInteractionListener mListener;
+
+    public MyFamilyRecyclerViewAdapter(List<Family> items, OnFamilyListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_family, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public Family mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.id);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
